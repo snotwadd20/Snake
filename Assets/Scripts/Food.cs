040266f2be1438f0bb7ba99240f230658ee28e72
Michael Stout <stout@uselessopinions.com>
2017-04-30 20:15:29 -0400

    Tails spawn when you eat food

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour 
+{
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		Snake snake = coll.gameObject.GetComponent<Snake>();
+		if (snake != null)
+		{
+			snake.ateFood = true;
+			Destroy(gameObject);
+		}//if
+	}//OnTriggerEnter2D
+		
+}//Food
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Snake : MonoBehaviour 
 {
+	public GameObject tailPrefab = null;
 
+	private List<Transform> tail = new List<Transform>();
 	private Vector2 dir = Vector2.right;
+
+	[HideInInspector]
+	public bool ateFood = false;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -35,6 +42,28 @@ public class Snake : MonoBehaviour
 
 	void Move()
 	{
+		Vector2 oldHeadPos = transform.position;
+
 		transform.Translate(dir);
+
+		if (ateFood)
+		{
+			//IF you ate food, put it a new tail the spot the head ysed to be
+			GameObject tailPiece = Instantiate(tailPrefab, oldHeadPos, Quaternion.identity);
+			tail.Insert(0, tailPiece.transform);
+			ateFood = false;
+		}//if
+		else if (tail.Count > 0)
+		{
+			//Otherwise move the last tail to where the head used to be
+			tail.Last().position = oldHeadPos;
+			tail.Insert(0, tail.Last());
+			tail.RemoveAt(tail.Count - 1);
+		}//if
 	}//Move
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		print("BAM");
+	}//OnCollisionEnter2D
 }//
